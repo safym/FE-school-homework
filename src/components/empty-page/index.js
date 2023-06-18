fa103e27a20a0edf52f59ca42b953036c0e87d79
safym <osafym@gmail.com>
2023-06-18 16:29:32 +0300

    add empty-page class component

---
+++ src/components/empty-page/index.js
@@ -0,0 +1,36 @@
+export class EmptyPage {
+  element;
+  subElement;
+
+  constructor() {
+    this.render()
+  }
+
+  render() {
+    this.element = this.getElement();
+    this.subElement = this.getSubElement(this.element);
+  }
+
+  getElement() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+
+    return element.firstElementChild;
+  }
+
+  getTemplate() {
+    return `
+    <div class="empty-page">
+      <p class="empty-page__label">Не создан ни один проект</p>
+      <a href="#" class="empty-page__button button button--style--primary">
+        Добавить
+      </a>
+    </div>
+    `
+  }
+
+  getSubElement(element) {
+    return element.firstElementChild
+  }
+}
+
