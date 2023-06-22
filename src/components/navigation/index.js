881480503c8aa92539abbd236d599c6328e74fee
safym <osafym@gmail.com>
2023-06-22 22:55:13 +0300

    add opening dropdown menu

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,12 +1,36 @@
+const MENU_BUTTON_ID = "user-button";
+
 export class Navigation {
+  dropdownButton;
+  dropdownIcon;
+  menuIsOpen;
+  menuElement;
+
   element;
   subElement;
 
   onClick = (event) => {
-    alert("navigation click");
+    const target = event.target;
+    const closestMenuButton = target.closest(`#${MENU_BUTTON_ID}`);
+
+    if (closestMenuButton) {
+      if (!this.menuIsOpen) {
+        this.openMenu();
+      } else {
+        if (this.menuElement) {
+          this.closeMenu();
+        }
+      }
+    } else {
+      if (this.menuIsOpen && this.menuElement) {
+        this.closeMenu();
+      }
+    }
   };
 
   constructor() {
+    this.menuIsOpen = false;
+
     this.render();
     this.initListeners();
   }
@@ -14,6 +38,7 @@ export class Navigation {
   render() {
     this.element = this.getElement();
     this.subElement = this.getSubElement(this.element);
+    this.dropdownButton = this.subElement.querySelector(`#${MENU_BUTTON_ID}`);
   }
 
   getElement() {
@@ -42,7 +67,7 @@ export class Navigation {
             >
           </li>
           <li class="header__list-item">
-            <a class="header__user-button button button__size_large button__dropdown" href="#">
+            <a id="${MENU_BUTTON_ID}" class="header__user-button button button__size_large button__dropdown dropdown" href="#">
               <img
                 class="button__image"
                 src="/assets/images/user.png"
@@ -61,11 +86,47 @@ export class Navigation {
     `;
   }
 
+  getMenuElement() {
+    const menu = document.createElement("div");
+    menu.innerHTML = this.getMenuTemplate();
+
+    return menu;
+  }
+
+  getMenuTemplate() {
+    return `
+    <div class="dropdown__menu">
+      <ul class="dropdown__list">
+        <li class="dropdown__list-item">
+          <a class="dropdown__link" href="#">Профиль</a>
+        </li>
+        <li class="dropdown__list-item">
+          <a class="dropdown__link" href="#">Выход</a>
+        </li>
+      </ul>
+    </div>
+    `;
+  }
+
+  openMenu() {
+    this.menuIsOpen = true;
+    this.menuElement = this.getMenuElement();
+
+    this.dropdownButton.parentElement.append(this.menuElement);
+    this.dropdownButton.classList.add("dropdown_active");
+  }
+
+  closeMenu() {
+    this.menuIsOpen = false;
+    this.dropdownButton.classList.remove("dropdown_active");
+    this.menuElement.remove();
+  }
+
   getSubElement(element) {
     return element.firstElementChild;
   }
 
   initListeners() {
-    this.subElement.addEventListener("click", this.onClick);
+    document.addEventListener("click", this.onClick);
   }
 }
