f0e7dfab095b99b0a6caae6ed1b6c8896b56aeec
safym <osafym@gmail.com>
2023-06-23 14:43:40 +0300

    fix data

--- src/data.js
+++ src/data.js
@@ -2,6 +2,8 @@ export const PROJECT_DATA = {
   id: "2653137653251836235432452513123124",
   description: `Очень длинное название проекта, создано специального для того чтоб не вместилось в одну строку, что позволит проверить не ломается ли верстка. И еще раз очень длинное название проекта, создано
   специально чтоб проверить не сломается ли верстка.` ,
+  status: null,
+  userImg: false,
   creator: "Иванов И.И.",
   createTime: "1 час назад",
   lastEditor: "Баранов В.В.",
@@ -9,9 +11,10 @@ export const PROJECT_DATA = {
 };
 
 export const TASK_DATA = {
-  id: "#1",
+  id: "1",
   description: "Название",
   status: "Черновик",
+  userImg: true,
   creator: "Иванов И.И.",
   createTime: "1 час назад",
   lastEditor: "Баранов В.В.",
