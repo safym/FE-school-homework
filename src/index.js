c828668d30c598a07954ba8d52cb891e2b969fc6
safym <osafym@gmail.com>
2023-06-26 13:01:00 +0300

    fix main index js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // import main index.html for webpack
-import html from "./index.html";
+import "./index.html";
 
 // import main style.scss for webpck
 import "./style.scss";
 
 // import images for webpack
-import user from "./assets/images/user.png";
+import "./assets/images/user.png";
 
 // import components
-import { Dropdown } from "./components/dropdown/index.js";
-import { Tooltip } from "./components/tooltip/index.js";
+import "./components/dropdown/index.js";
+import "./components/tooltip/index.js";
 import { EmptyPage } from "./components/empty-page/index.js";
 import { Navigation } from "./components/navigation/index.js";
 import { ProjectItem } from "./components/project-item/index.js";
@@ -19,37 +19,41 @@ import { TaskItem } from "./components/task-item/index.js";
 import { PROJECT_DATA, TASK_DATA, EMPTY_PAGE_DATA } from "./data.js";
 
 // Adding components to the app
-const app = document.getElementById("app");
+const renderApp = () => {
+  const app = document.getElementById("app");
 
-const taskContainer = document.createElement("div");
-taskContainer.id = "container";
+  const taskContainer = document.createElement("div");
+  taskContainer.id = "container";
 
-const navigation = new Navigation();
-const projectItem = new ProjectItem(PROJECT_DATA);
-const taskItem = new TaskItem(TASK_DATA);
-const emptyPage = new EmptyPage(EMPTY_PAGE_DATA);
+  const navigation = new Navigation();
+  const projectItem = new ProjectItem(PROJECT_DATA);
+  const taskItem = new TaskItem(TASK_DATA);
+  const emptyPage = new EmptyPage(EMPTY_PAGE_DATA);
 
-taskContainer.append(projectItem.element);
-taskContainer.append(taskItem.element);
-taskContainer.append(emptyPage.element);
+  taskContainer.append(projectItem.element);
+  taskContainer.append(taskItem.element);
+  taskContainer.append(emptyPage.element);
 
-app.append(navigation.element);
-app.append(taskContainer);
+  app.append(navigation.element);
+  app.append(taskContainer);
 
-// Global event listener for closing drop-down menus
-document.addEventListener("click", (event) => {
-  const closestButton = event.target.closest('[data-dropdown="button"]');
-  const closestList = event.target.closest('[data-dropdown="list"]');
-  const list = document.querySelector('[data-dropdown="list"]');
+  // Global event listener for closing drop-down menus
+  document.addEventListener("click", (event) => {
+    const closestButton = event.target.closest('[data-dropdown="button"]');
+    const closestList = event.target.closest('[data-dropdown="list"]');
+    const list = document.querySelector('[data-dropdown="list"]');
 
-  if (!closestButton && !closestList && list) {
-    switch (true) {
-      case navigation.menu.isOpen:
-        navigation.menu.close();
-      case projectItem.menu.isOpen:
-        projectItem.menu.close();
-      case taskItem.menu.isOpen:
-        taskItem.menu.close();
+    if (!closestButton && !closestList && list) {
+      switch (true) {
+        case navigation.menu.isOpen:
+          navigation.menu.close();
+        case projectItem.menu.isOpen:
+          projectItem.menu.close();
+        case taskItem.menu.isOpen:
+          taskItem.menu.close();
+      }
     }
-  }
-});
+  });
+};
+
+renderApp();
